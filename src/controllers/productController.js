bf8cefe93fb51c1d0ad33ded2ec66d9b8a333866
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2024-05-26 19:30:43 -0400

    Product 100%

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -6,10 +6,68 @@ controller.getProducts = (req, res) => {
             if (err) {
                 res.json(err);
             }
-            console.log(data);
-            res.render('products',{data: data});
+
+            console.log(data)
+
+            conn.query('SELECT * FROM Category', (err, categories) => {
+                if (err) {
+                    res.json(err);
+                }
+
+                console.log(categories);
+            res.render('products',{data: data, categories: categories});
+            });
         });
     });
 };
+
+controller.save = (req, res) => {
+    const data = req.body;
+    console.log(data);
+
+    req.getConnection((err, conn) => {
+        conn.query('INSERT INTO Product set ?', [data], (err, category) => {
+            res.redirect('/products');
+        });
+    });
+};
+
+controller.edit = (req, res) => {
+    const { id } = req.params;
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM Product WHERE id = ?', [id], (err, rows) => {
+            conn.query('SELECT * FROM Category', (err, categories) => {
+                if (err) {
+                    res.json(err);
+                }
+
+                console.log(categories);
+                res.render('products_edit', {
+                    data: rows[0],
+                    categories: categories
+                });
+        });
+        });
+    });
+}
+
+controller.update = (req, res) => {
+    const { id } = req.params;
+    const newProduct = req.body;
+    req.getConnection((err, conn) => {
+        conn.query('UPDATE Product set ? where id = ?', [newProduct, id], (err, rows) => {
+            res.redirect('/products');
+        });
+    });
+}   
+
+controller.delete = (req, res) => {
+    const { id } = req.params;
+    req.getConnection((err, connection) => {
+        connection.query('DELETE FROM Product WHERE id = ?', [id], (err, rows) => {
+            res.redirect('/products');
+        });
+    });
+}
 
 module.exports = controller;
--- src/routes/product.js
+++ src/routes/product.js
@@ -4,5 +4,9 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 
 router.get('/', productController.getProducts);
+router.post('/add', productController.save);
+router.get('/delete/:id', productController.delete);
+router.get('/edit/:id', productController.edit);
+router.post('/update/:id', productController.update);
 
 module.exports = router;
