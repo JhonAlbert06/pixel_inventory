4cb6279019cfa474ce3a5207bb188ddfc9ffd7ee
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2024-05-26 19:41:12 -0400

    Customer 50%

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -12,4 +12,44 @@ controller.getCustomers = (req, res) => {
     });
 };
 
+controller.save = (req, res) => {
+    const data = req.body;
+    req.getConnection((err, conn) => {
+        conn.query('INSERT INTO Customer set ?', [data], (err, customer) => {
+            console.log(customer);
+            res.redirect('/customers');
+        });
+    });
+};
+
+controller.edit = (req, res) => {
+    const { id } = req.params;
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM Customer WHERE id = ?', [id], (err, customer) => {
+            res.render('customer_edit', {
+                data: customer[0]
+            });
+        });
+    });
+};
+
+controller.update = (req, res) => {
+    const { id } = req.params;
+    const newCustomer = req.body;
+    req.getConnection((err, conn) => {
+        conn.query('UPDATE Customer set ? where id = ?', [newCustomer, id], (err, rows) => {
+            res.redirect('/customers');
+        });
+    });
+};
+
+controller.delete = (req, res) => {
+    const { id } = req.params;
+    req.getConnection((err, connection) => {
+        connection.query('DELETE FROM Customer WHERE id = ?', [id], (err, rows) => {
+            res.redirect('/customers');
+        });
+    });
+};
+
 module.exports = controller;
--- src/routes/customer.js
+++ src/routes/customer.js
@@ -4,5 +4,9 @@ const router = express.Router();
 const customerController = require('../controllers/customerController');
 
 router.get('/', customerController.getCustomers);
+router.post('/add', customerController.save);
+router.get('/delete/:id', customerController.delete);
+router.get('/edit/:id', customerController.edit);
+router.post('/update/:id', customerController.update);
 
 module.exports = router;
