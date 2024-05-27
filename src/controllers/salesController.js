63d33cd257aa7c265a39c825e8b8654a40b1ab2a
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2024-05-27 09:26:35 -0400

    Sales 20%

--- src/controllers/salesController.js
+++ src/controllers/salesController.js
@@ -12,4 +12,46 @@ controller.getSales = (req, res) => {
     });
 };
 
+
+controller.save = (req, res) => {
+    const data = req.body;
+    req.getConnection((err, conn) => {
+        conn.query('INSERT INTO Sale set ?', [data], (err, customer) => {
+            console.log(customer);
+            res.redirect('/sales');
+        });
+    });
+}
+
+controller.edit = (req, res) => {
+    const { id } = req.params;
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM Sale WHERE id = ?', [id], (err, rows) => {
+            res.render('sales_edit', {
+                data: rows[0]
+            });
+        });
+    });
+};
+
+controller.update = (req, res) => {
+    const { id } = req.params;
+    const newCustomer = req.body;
+    req.getConnection((err, conn) => {
+
+        conn.query('UPDATE Sale set ? where id = ?', [newCustomer, id], (err, rows) => {
+            res.redirect('/sales');
+        });
+    });
+};
+
+controller.delete = (req, res) => {
+    const { id } = req.params;
+    req.getConnection((err, connection) => {
+        connection.query('DELETE FROM Sale WHERE id = ?', [id], (err, rows) => {
+            res.redirect('/sales');
+        });
+    });
+};
+
 module.exports = controller;
--- src/routes/sales.js
+++ src/routes/sales.js
@@ -4,5 +4,9 @@ const router = express.Router();
 const salesController = require('../controllers/salesController');
 
 router.get('/', salesController.getSales);
+router.post('/add', salesController.save);
+router.get('/delete/:id', salesController.delete);
+router.get('/edit/:id', salesController.edit);
+router.post('/update/:id', salesController.update);
 
 module.exports = router;
